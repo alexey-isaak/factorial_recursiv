68ef8e5ffa4d89f0672cd35e7486d5eeb99e427f
alex-isaak <122435364+alex-isaak@users.noreply.github.com>
2023-02-13 09:49:15 +0300

    Factorial Recursive

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double Factorial (double n)
+{
+    if (n == 1 || n == 0) return 1;
+    else return n * Factorial (n-1);
+}
+
+for (int i  = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+//Console.Write(Factorial(3));
